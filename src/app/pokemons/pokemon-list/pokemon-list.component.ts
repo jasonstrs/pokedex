0c27830ac4b1b9e49dc5a61260dc123c4864b488
Jason Sautieres <jason.sautieres@gmail.com>
2022-02-09 18:40:57 +0100

    first-part-pokedex: Display the list of Pokemons

---
+++ src/app/pokemons/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../models/pokemon.model';
+import { PokemonService } from '../services/pokemon.service';
+
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.scss']
+})
+export class PokemonListComponent implements OnInit {
+  pokemons?: Pokemon[];
+
+  constructor(private pokemonService: PokemonService) { }
+
+
+  ngOnInit(): void {
+    this.pokemonService.getPokemons().subscribe(({ data }) => this.pokemons = data)
+  }
+
+
+
+}
---
+++ src/app/pokemons/services/pokemon.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { MessageService } from 'src/app/services/message.service';
+import { Pokemon } from '../models/pokemon.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PokemonService {
+  private pokemonsUrl : string = "https://app-ec21e68e-3e55-42d7-b1ae-3eef7507a353.cleverapps.io/pokemons"
+
+  constructor(private messageService : MessageService, private http: HttpClient) { }
+
+  getPokemons(): Observable<any> {
+    return this.http.get<any>(this.pokemonsUrl).pipe(
+      tap(() => this.log("fetched Pokemons")),
+      catchError(this.handleError<any>('getPokemons', [])),
+    );
+  }
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T): (error: any) => Observable<T> {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
